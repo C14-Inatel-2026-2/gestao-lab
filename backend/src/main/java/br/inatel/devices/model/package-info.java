f508c02872b7f7fc799a8716dfa5aea65d7cf2a0
Giovana Franciele <giovana.leite@ges.inatel.br>
2026-09-11 23:09:15 -0300

    Adicionando um modelo basico e o teste

--- backend/src/main/java/br/inatel/devices/model/package-info.java
+++ backend/src/main/java/br/inatel/devices/model/package-info.java
@@ -1,1 +1,19 @@
 package br.inatel.devices.model;
+
+public class Device {
+    private String nome;
+    private String status;
+
+    public Device(String nome) {
+        this.nome = nome;
+        this.status = "DISPONIVEL";
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
--- backend/src/test/java/br/inatel/devices/package-info.java
+++ backend/src/test/java/br/inatel/devices/package-info.java
@@ -1,1 +1,14 @@
 package br.inatel.devices;
+
+import br.inatel.devices.model.Device;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DeviceTest {
+
+    @Test
+    void testeCriarDispositivo() {
+        Device d = new Device("Osciloscopio");
+        assertEquals("DISPONIVEL", d.getStatus());
+    }
+}

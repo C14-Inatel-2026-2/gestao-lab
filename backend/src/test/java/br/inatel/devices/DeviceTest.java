5c52045ce30e050220d2d26f4d55dc996a90f8cc
lucas <lucasynoguti@gmail.com>
2026-09-12 00:35:09 -0300

    refactor(devices): split DeviceModel into Device and DeviceStatus, fix test to use the implemented enum

--- backend/src/test/java/br/inatel/devices/DeviceTest.java
+++ backend/src/test/java/br/inatel/devices/DeviceTest.java
@@ -1,14 +1,22 @@
 package br.inatel.devices;
 
 import br.inatel.devices.model.Device;
+import br.inatel.devices.model.DeviceStatus;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DeviceTest {
 
     @Test
-    void testeCriarDispositivo() {
-        Device d = new Device("Osciloscopio");
-        assertEquals("DISPONIVEL", d.getStatus());
+    void shouldStartAsAvailable() {
+        Device device = new Device("Oscilloscope");
+        assertEquals(DeviceStatus.AVAILABLE, device.getStatus());
+    }
+
+    @Test
+    void shouldAllowStatusChange() {
+        Device device = new Device("Oscilloscope");
+        device.setStatus(DeviceStatus.BORROWED);
+        assertEquals(DeviceStatus.BORROWED, device.getStatus());
     }
 }
